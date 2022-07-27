3e0d96ee3244dfe8f1839a4326aba67e4863c61c
codeAdventureNow <nmueller855@gmail.com>
2022-07-27 01:38:11 -0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+  return (
+    <div>
+      {/*Search Component  */}
+      <UserResults />
+    </div>
+  );
+}
+
+export default Home;
